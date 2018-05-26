1ac7a542878dd246b728d23b31d2c9f968561fe6
AUIRtz <35659203+AUIRtz@users.noreply.github.com>
2018-05-26 17:42:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
+document.body.ontouchstart = function(cancel){
+    cancel.preventDefault()
+}
 autoSetCanvasSize(canvas)
 monitorUser(canvas)
 var lineWidth = 5
